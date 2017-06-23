36d557d8412b9eb5e5a581da0adaed70b785f69f
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-22 21:45:22 -0700

    Create Loader.php

---
+++ plugins/Loader.php
@@ -0,0 +1,52 @@
+<?php 
+
+include_once('plugins/Event.php');
+include_once('plugins/Hook.php');
+
+function Loader($dir = 'plugins',$DieOnFail = true){
+  Event('Before Loading Directory: '.$dir);
+  if($dir=='plugins'){
+  
+    if($handle = opendir('plugins')){
+      while (false !== ($class = readdir($handle))){
+        $include_path='plugins/'.$class;
+        if((!(strpos($class,'.php')===false)) && $class != "."&& $class != "index.php" && $class != ".." && file_exists($include_path)){
+          Event('Before Loading: '.$include_path);
+          include_once($include_path);
+          Event('After Loading: '.$include_path);
+        }
+      }
+      closedir($handle);
+    }
+  
+  }else{
+    
+    if(
+      (!(file_exists($dir)))&&
+      $DieOnFail
+    ){
+      die('Loader could not find dir: '.$dir);
+    }
+    if(file_exists($include_path=$dir.'/Routing.php')){
+      Event('Before Loading: '.$include_path);
+      include_once($include_path);
+      Event('After Loading: '.$include_path);
+    }else{
+      Event('Could not find: '.$include_path);
+    }
+    
+    if($handle = opendir($dir)){
+      while (false !== ($extension = readdir($handle))){
+        $Path=$dir.'/'.$extension;
+        if($extension != "." && $extension != ".." && is_dir($Path)){
+          Event('Before Recursively Loading Subdirectory: '.$extension);
+          Loader($Path);
+          Event('After Recursively Loading Subdirectory: '.$extension);
+        }
+      }
+      closedir($handle);
+    }
+    
+  }
+  Event('After Loading Directory: '.$dir);
+}
