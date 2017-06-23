307705b3e382d1835ef49f4386b1c53fff83ce98
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-22 23:32:28 -0700

    Update Fetch.php

--- Fetch.php
+++ Fetch.php
@@ -7,7 +7,7 @@ include('plugins/Loader.php');
 Loader('plugins');
 
 $URL = $_GET['url'];
-$Links = CrawlSiteForLinks($URL,2,'rss');
+$Links = CrawlDomainForLinks($URL,2,'rss');
 pd($Links);
 exit;
 
