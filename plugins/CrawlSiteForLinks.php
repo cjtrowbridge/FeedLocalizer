2adf58c54ad5f45659af981503b828a109bd3cc3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-22 22:44:25 -0700

    Create CrawlSiteForLinks.php

---
+++ plugins/CrawlSiteForLinks.php
@@ -0,0 +1,54 @@
+<?php
+
+function CrawlSiteForLinks($URL,$Depth = 5){
+  global $CrawlSiteLinks;
+  $CrawlSiteLinks=array();
+  
+  crawl_page_recursive($URL, $Depth);
+  
+  $Temp = $CrawlSiteLinks;
+  unset($CrawlSiteLinks);
+  return $Temp;
+}
+
+function crawl_page_recursive($url, $depth = 5){
+  set_time_limit(0);
+  $seen = array();
+  if(($depth == 0) or (in_array($url, $seen))){
+      return;
+  }   
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+  $result = curl_exec ($ch);
+  curl_close ($ch);
+  if( $result ){
+      $stripped_file = strip_tags($result, "<a>");
+      preg_match_all("/<a[\s]+[^>]*?href[\s]?=[\s\"\']+"."(.*?)[\"\']+.*?>"."([^<]+|.*?)?<\/a>/", $stripped_file, $matches, PREG_SET_ORDER ); 
+      foreach($matches as $match){
+          $href = $match[1];
+              if (0 !== strpos($href, 'http')) {
+                  $path = '/' . ltrim($href, '/');
+                  if (extension_loaded('http')) {
+                      $href = http_build_url($href , array('path' => $path));
+                  } else {
+                      $parts = parse_url($href);
+                      $href = $parts['scheme'] . '://';
+                      if (isset($parts['user']) && isset($parts['pass'])) {
+                          $href .= $parts['user'] . ':' . $parts['pass'] . '@';
+                      }
+                      $href .= $parts['host'];
+                      if (isset($parts['port'])) {
+                          $href .= ':' . $parts['port'];
+                      }
+                      $href .= $path;
+                  }
+              }
+              crawl_page_recursive($href, $depth - 1);
+          }
+  }   
+  //echo "Crawled {$href}";
+  global $CrawlSiteLinks;
+  $CrawlSiteLinks[]=$href;
+}
