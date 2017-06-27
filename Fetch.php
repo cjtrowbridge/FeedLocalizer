cf6e3dc3454a32ec99ffec2e5076d98844482e26
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-27 16:25:32 -0700

    Update Fetch.php

--- Fetch.php
+++ Fetch.php
@@ -6,10 +6,12 @@ ini_set('display_errors', '1');
 include('plugins/Loader.php');
 Loader('plugins');
 
+/*
 $URL = $_GET['url'];
 $Links = CrawlDomainForLinks($URL,2,'rss');
 pd($Links);
 exit;
+*/
 
 Loader('api/v1');
 
