9cd04c8271d1ae20aafa9312c8fa7219935f19b1
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-06-27 17:08:18 -0700

    Update and rename Fetch.php to index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require('config.php');
+include('plugins/Loader.php');
+Loader('plugins');
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>You are probably in the wrong place.</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+  
+</head>
+<body>
+  <div class="container no-gutters">
+    <br>
+    <div class="card">
+      <div class="card-block">
+        <h1 class="card-title"><center>You are probably in the wrong place.</center></h1>
+      </div>
+    </div>
+    
+  </div><!-- /.container -->
+
+</body>
+</html>
